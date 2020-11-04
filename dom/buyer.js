d772dc893847b69080c6cac632ec8856cc854182
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2020-11-04 14:07:17 +0200

    i dont know

--- dom/buyer.js
+++ dom/buyer.js
@@ -1,19 +1,39 @@
-// createPodactBlock
-// EG OBJICT = {id:"1", name:"Doe", price:46}
-function createHTMLForObj(obj){
-    let newObj = document.createElement('div');
-    const objProps = Object.keys(obj);
-    const objPropsValues = Object.values(obj);
-    for(let i =0; i < objProps.length; i++){
-        let objProP = document.createElement('p');
-        let objProPC = document.createTextNode(objProps[i]);
-        objProP.appendChild(objProPC);
-        let objProV = document.createElement('p');
-        let objProVC = document.createTextNode(objPropsValues[i]);
-        objProV.appendChild(objProVC);
-        newObj.appendChild(objProP);
-        newObj.appendChild( objProV);
-    }
-    return newObj;
-} 
+function createHTMLForObj(obj) {
+  if (!obj) return;
+  let newObj = document.createElement("div");
+  newObj.setAttribute('class', 'test');
+  const objProps = Object.keys(obj);
+  objProps.shift();
+  objProps.forEach((prop) => {
+    let objProP = document.createElement("p");
+    let objProPC = document.createTextNode(prop);
+    objProP.appendChild(objProPC);
+    let objProV = document.createElement("p");
+    let objProVC = document.createTextNode(obj[prop]);
+    objProV.appendChild(objProVC);
+    newObj.appendChild(objProP);
+    newObj.appendChild(objProV);
+    let newBtn = document.createElement("button");
+    newBtn.setAttribute('class', 'addToCartBtn');
+    let objID = obj.id;
+    newBtn.addEventListener("click" /*function.js*/);
+  });
+  return newObj;
+}
 
+function filterFunc(cate) {
+  let products = localStorage.getItem("/*products array*/");
+  document.getElementByClassName("productContainer").innerHTML = "";
+  for (i = 0; i < products.length; i++) {
+    if (products[i][category] == cate) {
+      let newProd = createHTMLForObj(products[i]);
+      document.getElementByClassName("productContainer").appendChild(newProd);
+    }
+  }
+  //gets array from local storage
+  //clears products container
+  //loops over the array
+  //checks if the chosen category matches the products category
+  //creates the matching objects' HTML
+  //adds them to products container
+}
